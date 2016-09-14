579f42b6534a0d381559fdd9e5d441d3723a9b1b
Tej99 <kieranwardle95@gmail.com>
2016-09-14 10:30:49 +0000

    everything works except jira

--- bootstrap_master.sh
+++ bootstrap_master.sh
@@ -13,7 +13,7 @@ sudo apt-get install -y openssh-server openssh-client
 sudo ufw disable
 
 # Install puppet
-sudo apt-get install -y puppet puppetmaster
+sudo apt-get install -y puppet puppetmaster 
 
 # Adding lines in hosts file: (/etc/hosts)
 # This inserts 2 lines in the host.
@@ -34,7 +34,8 @@ sudo puppet cert sign --all
 sudo puppet module install puppetlabs-java -i /etc/puppet/modules
 sudo puppet module install maestrodev-maven -i /etc/puppet/modules
 sudo puppet module install puppetlabs-git -i /etc/puppet/modules
-sudo puppet module install mkrakowitzer-jira -i /etc/puppet/modules
+#sudo puppet module install puppet-jira -i /etc/puppet/modules
+#sudo puppet module install mkrakowitzer-jira -i /etc/puppet/modules
 sudo puppet module install rtyler-jenkins -i /etc/puppet/modules
 #sudo puppet module install puppetlabs-java
 
@@ -47,11 +48,9 @@ echo "class { 'java':" | sudo tee --append /etc/puppet/manifests/site.pp 2> /dev
 echo "  distribution => 'jdk'," | sudo tee --append /etc/puppet/manifests/site.pp 2> /dev/null && \
 echo "  version      => 'latest'," | sudo tee --append /etc/puppet/manifests/site.pp 2> /dev/null && \
 echo "}" | sudo tee --append /etc/puppet/manifests/site.pp 2> /dev/null && \
-echo "class { 'jira':" | sudo tee --append /etc/puppet/manifests/site.pp 2> /dev/null && \
-echo "javahome    => '/opt/java'," | sudo tee --append /etc/puppet/manifests/site.pp 2> /dev/null && \
-echo "}" | sudo tee --append /etc/puppet/manifests/site.pp 2> /dev/null && \
 echo "include jenkins" | sudo tee --append /etc/puppet/manifests/site.pp 2> /dev/null && \
 echo "include git" | sudo tee --append /etc/puppet/manifests/site.pp 2> /dev/null && \
+echo "include maven" | sudo tee --append /etc/puppet/manifests/site.pp 2> /dev/null && \
 echo "#include tomcat" | sudo tee --append /etc/puppet/manifests/site.pp 2> /dev/null && \
 echo "}" | sudo tee --append /etc/puppet/manifests/site.pp 2> /dev/null
 
@@ -60,8 +59,14 @@ echo "}" | sudo tee --append /etc/puppet/manifests/site.pp 2> /dev/null
 
 
 
+#echo "class { 'jenkins':" | sudo tee --append /etc/puppet/manifests/site.pp 2> /dev/null && \
+#echo "config_hash  => { 'HTTP_PORT' => { 'value' => 8081 } }," | sudo tee --append /etc/puppet/manifests/site.pp 2> /dev/null && \
+#echo "}" | sudo tee --append /etc/puppet/manifests/site.pp 2> /dev/null && \
 
 
-
+#echo "class { 'jira':" | sudo tee --append /etc/puppet/manifests/site.pp 2> /dev/null && \
+#echo "javahome    => '/opt/java'," | sudo tee --append /etc/puppet/manifests/site.pp 2> /dev/null && \
+#echo "version     => '6.3.7'," | sudo tee --append /etc/puppet/manifests/site.pp 2> /dev/null && \
+#echo "}" | sudo tee --append /etc/puppet/manifests/site.pp 2> /dev/null && \
 
 
